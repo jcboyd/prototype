6017570388413b7cee80dd7736307c8e0f84d073
jcboyd <boyd.joseph.c@gmail.com>
2014-10-02 00:53:26 +0200

    XAMPP

---
+++ connection.php
@@ -0,0 +1,21 @@
+<?php
+
+$user = 'root';
+$pass = '';
+$db = 'testdb';
+
+$con = mysql_connect('localhost', $user, $pass);
+
+if (!$con) {
+	die('Could not connect: ' . mysql_error());
+}
+
+$db_selected = mysql_select_db($db, $con);
+
+$sql = "SELECT name FROM kamusi WHERE id = 1";
+$result = mysql_query($sql, $con);
+
+echo mysql_result($result, 0);
+mysql_close($con);
+
+?>
